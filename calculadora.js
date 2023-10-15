3fe94767641047aeb76755e0f8d47f3d4ecff75a
christianfelps <christianfelipialvim@gmail.com>
2023-10-15 07:59:05 -0300

    calculadora feita

---
+++ calculadora.js
@@ -0,0 +1,24 @@
+var rank = 0
+let saldoDeVitorias = calcular(335, 225)
+console.log( "o heroi tem de saldo de " + saldoDeVitorias + " esta no nivel " +  rank)
+    function  calcular(vitoria, derrota ){
+        
+        let result = vitoria - derrota
+            if( vitoria <= 9 ){
+                rank = "Ferro"
+           }else if (result <= 20){
+              rank = "Bronze"  
+          }else if(result <= 50){
+              rank = "Prata" 
+          }else if(result <= 80){
+              rank = "Ouro" 
+          }else if(result <= 90){
+              rank= "diamante"
+          }else if( result <= 100){
+              rank = "Lendario"
+          }else if (result >= 101){
+              rank = " Imortal"
+                        } 
+            return result
+            
+}
